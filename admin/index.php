5f5b5d656f671715137d660e341f2212d62ba2e4
quandoan2004 <quandqph37253@fpt.edu.vn>
2024-01-26 23:34:08 +0700

    xoadm

--- admin/index.php
+++ admin/index.php
@@ -15,13 +15,24 @@ if (isset($_GET['act'])) {
             include "danhmuc/add.php";
             break;
         case 'lisdm':
-            $spl="select * from danhmuc order by name";
+            $spl="select * from danhmuc order by id desc";
             $listdanhmuc = pdo_query($spl);
             include "danhmuc/list.php";
             break;
-        default:
-            include "home.php";
-            break;
+        case 'xoadm':
+                if(isset($_GET['id'])&&($_GET['id']>0)){
+                    $spl= "delete  from danhmuc where id=".$_GET['id'];
+                    pdo_execute($spl);
+                }
+                $spl="select * from danhmuc order by id desc";
+                $listdanhmuc=pdo_query($spl);
+                include "danhmuc/list.php";
+                break;
+
+
+            default:
+                include "home.php";
+                break;
     }
 }else {
     include "home.php";
