ca6a5c7c527c66d553068eb1da955278b453bf3a
quandoan2004 <quandqph37253@fpt.edu.vn>
2024-01-26 21:46:42 +0700

    admin

---
+++ admin/home.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="row frmtitle">
+        <h1>Thêm mới hàng hóa</h1>
+    </div>
+    <div class="row frmcontent">
+        <form action="#" method="post">
+            <div class="row mb10">
+                Mã loại <br>
+                <input type="text" name="maloai" disabled>
+            </div>
+            <div class="row mb10">
+                Tên loại <br>
+                <input type="text" name="tên loại">
+            </div>
+            <div class="row mb10">
+                <input type="submit" value="THÊM MỚI">
+                <input type="reset" value="NHẬP LẠI">
+                <a href="/danhsach_loaihang.html"><input type="button" value="DANH SÁCH"></a>
+            </div>
+        </form>
+    </div>
+</div>
