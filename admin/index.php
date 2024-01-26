b1d30c898f1a3e5356081c964c335f2d960d8135
quandoan2004 <quandqph37253@fpt.edu.vn>
2024-01-27 00:06:08 +0700

    suadm

--- admin/index.php
+++ admin/index.php
@@ -28,8 +28,26 @@ if (isset($_GET['act'])) {
                 $listdanhmuc=pdo_query($spl);
                 include "danhmuc/list.php";
                 break;
+        case 'suadm':
+                if(isset($_GET['id'])&&($_GET['id']>0)){
+                    $spl="select * from danhmuc where id=".$_GET['id'];
+                    $dm=pdo_query_one($spl);
 
-
+                }
+                include "danhmuc/update.php";
+                break;
+        case 'updatedm':
+                if(isset($_POST['capnhat'])&&($_POST['capnhat']>0)){
+                    $tenloai=$_POST['tenloai'];
+                    $id=$_POST['id'];
+                    $spl="update danhmuc set name='".$tenloai."' where id = ".$id;
+                    pdo_execute($spl);
+                    $thongbao="Cập nhật thành công";
+                }
+                $spl="select * from danhmuc order by id desc";
+                $listdanhmuc=pdo_query($spl);
+                include "danhmuc/list.php";
+                break;
             default:
                 include "home.php";
                 break;
