726dee11ebd50124ccc932150d3f0f805b3434a2
quandoan2004 <quandqph37253@fpt.edu.vn>
2024-01-28 00:45:30 +0700

    htmltrangchu

--- view/home.php
+++ view/home.php
@@ -2,7 +2,40 @@
             <div class="boxtrai mr">
                 <div class="row">
                     <div class="banner">
-                        <img src="view/images/1000.jpg" alt="">
+                        <!-- Slideshow container -->
+<div class="slideshow-container">
+
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="view/images/bannner1.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="view/images/bannner2.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="view/images/banner3.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div>
 
                     </div>
                 </div>
